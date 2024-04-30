e90b4f4d2980f6ab60c8a19cef23ede981d47ee3
Erick Yoshike <ericktakao28@gmail.com>
2024-04-30 15:54:02 -0300

    Finishing SQL context implementation

--- 03_RestWithASPNET_ControllerModel/RestWithASPNET/RestWithASPNET/Services/Implementations/PersonServiceImplementation.cs
+++ 03_RestWithASPNET_ControllerModel/RestWithASPNET/RestWithASPNET/Services/Implementations/PersonServiceImplementation.cs
@@ -1,5 +1,6 @@
 ﻿using RestWithASPNET.Model;
 using RestWithASPNET.Model.Context;
+using System;
 
 namespace RestWithASPNET.Services.Implementations
 {
@@ -14,12 +15,16 @@ namespace RestWithASPNET.Services.Implementations
 
         public Person Create(Person person)
         {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
             return person;
-        }
-
-        public void Delete(long id)
-        {
-           
         }
 
         public List<Person> FindAll()
@@ -29,19 +34,49 @@ namespace RestWithASPNET.Services.Implementations
 
         public Person FindByID(long id)
         {
-            return new Person
-            {
-                Id = id,
-                FirstName = "Erick",
-                LastName = "Yoshike",
-                Address = "Álvares Machado - SP",
-                Gender = "Male"
-            };
+            return _context.People.SingleOrDefault(p => p.Id.Equals(id));
         }
 
         public Person Update(Person person)
         {
+            if (!Exists(person.Id)) return new Person();
+
+            var result = _context.People.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
             return person;
+        }
+
+        public void Delete(long id)
+        {
+            var result = _context.People.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.People.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        private bool Exists(long id)
+        {
+            return _context.People.Any(p => p.Id.Equals(id));
         }
     }
 }
