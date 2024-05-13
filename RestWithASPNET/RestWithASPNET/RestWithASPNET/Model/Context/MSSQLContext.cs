84073c136f44aeb9e9d65ffe96c1d118de58c831
Rafael Franco <Rafael.Franco@e-hps.com>
2024-05-13 15:51:43 -0300

    Creating User model

--- RestWithASPNET/RestWithASPNET/RestWithASPNET/Model/Context/MSSQLContext.cs
+++ RestWithASPNET/RestWithASPNET/RestWithASPNET/Model/Context/MSSQLContext.cs
@@ -14,5 +14,6 @@ namespace RestWithASPNET.Model.Context
 
         public DbSet<Person> People { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
