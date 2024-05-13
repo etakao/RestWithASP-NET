19afdd333a32db27284d9321532ec8dfb0f683da
Rafael Franco <Rafael.Franco@e-hps.com>
2024-05-13 16:08:02 -0300

    Creating User repository

---
+++ RestWithASPNET/RestWithASPNET/RestWithASPNET/Repository/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using RestWithASPNET.Data.VO;
+using RestWithASPNET.Model;
+using RestWithASPNET.Model.Context;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RestWithASPNET.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MSSQLContext _context;
+
+        public UserRepository(MSSQLContext context)
+        {
+            _context = context;
+        }
+
+        public User ValidadeCredentials(UserVO user)
+        {
+            var pass = ComputeHash(user.Password, SHA256.Create());
+            return _context.Users.FirstOrDefault(u => (u.UserName == user.UserName) && (u.Password == pass));
+        }
+
+        private string ComputeHash(string input, HashAlgorithm algorithm)
+        {
+            byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+            byte[] hashedBytes = algorithm.ComputeHash(inputBytes);
+            
+            var sBuilder = new StringBuilder();
+
+            foreach (var item in hashedBytes)
+            {
+                sBuilder.Append(item.ToString("x2"));
+            }
+
+            return BitConverter.ToString(hashedBytes);
+        }
+    }
+}
