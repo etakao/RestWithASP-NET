6722d5bd906d39f225fedf73ff29115d97d19d66
Rafael Franco <Rafael.Franco@e-hps.com>
2024-05-13 16:42:23 -0300

    Implementing Token Service continuation

--- RestWithASPNET/RestWithASPNET/RestWithASPNET/Services/Implementations/TokenService.cs
+++ RestWithASPNET/RestWithASPNET/RestWithASPNET/Services/Implementations/TokenService.cs
@@ -2,6 +2,7 @@
 using RestWithASPNET.Configurations;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace RestWithASPNET.Services.Implementations
@@ -35,12 +36,33 @@ namespace RestWithASPNET.Services.Implementations
 
         public string GenerateRefreshToken()
         {
-            throw new NotImplementedException();
+            var randomNumber = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                return Convert.ToBase64String(randomNumber);
+            }
         }
 
         public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
         {
-            throw new NotImplementedException();
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.Secret)),
+                ValidateLifetime = false
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken securityToken;
+
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+            if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCulture))
+                throw new SecurityTokenException("Invalid token");
+
+            return principal;
         }
     }
 }
