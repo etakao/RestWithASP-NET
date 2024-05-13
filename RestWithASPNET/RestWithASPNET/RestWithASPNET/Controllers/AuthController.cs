f232524a786c5d3a7d63f438f45fea749164d5a7
Rafael Franco <Rafael.Franco@e-hps.com>
2024-05-13 17:17:00 -0300

    Creating AuthController

---
+++ RestWithASPNET/RestWithASPNET/RestWithASPNET/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestWithASPNET.Business;
+using RestWithASPNET.Data.VO;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/[controller]/v{version:apiVersion}")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private ILoginBusiness _loginBusiness;
+
+        public AuthController(ILoginBusiness loginBusiness)
+        {
+            _loginBusiness = loginBusiness;
+        }
+
+        [HttpPost]
+        [Route("signin")]
+        public IActionResult SignIn([FromBody] UserVO user)
+        {
+            if (user == null) return BadRequest("Invalid client request.");
+
+            var token = _loginBusiness.ValidateCrendentials(user);
+            if (token == null) return Unauthorized();
+
+            return Ok(token);
+        }
+    }
+}
